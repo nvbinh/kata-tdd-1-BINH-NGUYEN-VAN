c65e90b8cc7056aff392494c6b7641759f05d621
Bing Nguyen <bingnguyen@gmail.com>
2015-09-07 21:36:08 +0700

    [master] Test Case 2: Pass string has value 1 refactor

--- app/calculator.js
+++ app/calculator.js
@@ -5,19 +5,25 @@
 */
 var cal = (function(my) {
 	var _private = 	{
-		Add: function(numbers) {
-			var result = 0, arrayTemp;
-			/*Case 1: string is empty*/
-			if (numbers.length <= 0) {
-				result = 0;
-				return result;
-			}
-
-			arrayTemp = numbers.split(',').map(Number);
+		commasArray: function(numbers) {
+			var result = 0,
+				arrayTemp = numbers.split(',').map(Number);
 
 			for (var i in arrayTemp) {
 				result += arrayTemp[i];
 			}
+			return result;
+		},
+
+		Add: function(numbers) {
+			var result = 0;
+			/*Test Case 1: string is empty*/
+			if (numbers.length <= 0) {
+				return 0;
+			}
+
+			/*Test Case 2: string is has value 1*/
+			result = _private.commasArray(numbers);
 
 			return result;
 		}
