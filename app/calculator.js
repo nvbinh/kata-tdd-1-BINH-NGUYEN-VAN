e93daf5ee5a086ef391254c6bf05008e61acecef
Bing Nguyen <bingnguyen@gmail.com>
2015-09-08 23:23:31 +0700

    [master] Test case 5: Support different delimiters implementation

--- app/calculator.js
+++ app/calculator.js
@@ -6,6 +6,14 @@
 var cal = (function(my) {
 	var _private = 	{
 		separators: [' ', '-', '\n', ';', ','],
+
+		hasMultiDelimiter: function(numbers) {
+			var pattern = numbers.substring(0, 2);
+			if (pattern === '//') {
+				return true;
+			}
+			return false;
+		},
 		
 		commasArray: function(numbers) {
 			var result = 0,
@@ -18,12 +26,16 @@ var cal = (function(my) {
 		},
 
 		Add: function(numbers) {
-			var result = 0;
+			var result = 0, multi;
 			/*Test Case 1: string is empty*/
 			if (numbers.length <= 0) {
 				return 0;
 			}
 
+			multi = _private.hasMultiDelimiter(numbers);
+			if (multi) {
+				numbers = numbers.substring(2, numbers.length);
+			}
 			/*Test Case 2: string is has value 1*/
 			/*Test Case 3: string is has value 1,2*/
 			result = _private.commasArray(numbers);
